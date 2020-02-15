cf5d597881e50c38e568cdda13153c155f2daf0f
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-02-15 13:48:26 -0500

    Add initialize_job_state stored procedure.

---
+++ SQL-Agent-Job-State/SQL-Agent-Job-State-SQL/Stored Procedures/initialize_job_state.sql
@@ -0,0 +1,37 @@
+﻿CREATE PROCEDURE [dbo].[initialize_job_state]
+	@p_job_id	NVARCHAR(50)
+,	@p_state	NVARCHAR(4000)
+AS
+BEGIN
+	DECLARE 
+		@job_id		UNIQUEIDENTIFIER = TRY_CONVERT(UNIQUEIDENTIFIER, @p_job_id)
+	,	@error_msg	NVARCHAR(MAX);
+
+	IF @job_id IS NULL
+	BEGIN
+		SET @error_msg = CONCAT(
+		 	@p_job_id
+		,	N' is not a valid Job ID.'
+		);
+		THROW 2147483647, @error_msg, 1; 
+	END
+
+	UPDATE [dbo].[job_state]
+	SET [state] = @p_state
+	WHERE job_id = @job_id
+	AND [server_name] = @@SERVERNAME;
+
+	IF @@ROWCOUNT = 0
+	BEGIN
+		INSERT [dbo].[job_state] (
+			[job_id]
+		,	[server_name]
+		,	[state]
+		)
+		VALUES (
+			@job_id
+		,	@@SERVERNAME
+		,	@p_state
+		)
+	END
+END
