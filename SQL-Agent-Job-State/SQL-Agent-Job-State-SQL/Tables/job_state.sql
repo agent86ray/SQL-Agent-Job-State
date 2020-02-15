fe6499d693f350d2007c0b1b74584fd7c9bbab55
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-02-15 07:29:16 -0500

    Create [dbo].[job_state] table.

---
+++ SQL-Agent-Job-State/SQL-Agent-Job-State-SQL/Tables/job_state.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[job_state]
+(
+	[id]			INT IDENTITY		NOT NULL 
+,	[server_name]	NVARCHAR(128)		NOT NULL
+,	[job_id]		UNIQUEIDENTIFIER	NOT NULL
+,	[state]			NVARCHAR(4000)		NULL
+)
