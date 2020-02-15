6c422a57abfc1defd4c898be35d174ecc000d966
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-02-15 13:47:57 -0500

    Add clustered primary key.

--- SQL-Agent-Job-State/SQL-Agent-Job-State-SQL/Tables/job_state.sql
+++ SQL-Agent-Job-State/SQL-Agent-Job-State-SQL/Tables/job_state.sql
@@ -4,4 +4,6 @@
 ,	[server_name]	NVARCHAR(128)		NOT NULL
 ,	[job_id]		UNIQUEIDENTIFIER	NOT NULL
 ,	[state]			NVARCHAR(4000)		NULL
+,	CONSTRAINT pkc_job_state PRIMARY KEY CLUSTERED
+		([job_id], [server_name])
 )
