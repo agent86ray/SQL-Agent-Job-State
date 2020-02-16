553b03cb415710f827ba88192a5f13d379db92cd
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-02-16 06:15:54 -0500

    Add script to get selected SQL Agent token values.

---
+++ SQL-Agent-Job-State/SQL-Agent-Job-State-SQL/Scripts/sql_agent_tokens.sql
@@ -0,0 +1,18 @@
+﻿-- copy/paste this script in a SQL Agent job step
+-- to see the tokens and their values
+INSERT [dbo].[sql_agent_token_value] (
+	[token]	
+,	[value]	
+)
+SELECT
+	N'JOBID'
+,	CONVERT(NVARCHAR(50), CONVERT(uniqueidentifier, $(ESCAPE_NONE(JOBID))))
+
+UNION ALL
+
+SELECT
+	N'STEPID'
+,	CONVERT(NVARCHAR(50), $(ESCAPE_SQUOTE(STEPID)))
+
+
+
