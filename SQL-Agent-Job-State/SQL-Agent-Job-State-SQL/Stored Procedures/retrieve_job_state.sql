b52531c472ceed2053628fa4dc9aab5f7fd08555
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-02-16 18:02:55 -0500

    Revise to query based on Id.

---
+++ SQL-Agent-Job-State/SQL-Agent-Job-State-SQL/Stored Procedures/retrieve_job_state.sql
@@ -0,0 +1,19 @@
+﻿/*
+
+	Retrieve the job state for a SQL Agent job. The @p_job_state_id parameter
+	should come from the SSIS environment with the same name as the SQL Agent
+	job.
+
+*/
+CREATE PROCEDURE [dbo].[retrieve_job_state]
+	@p_job_state_id	INT
+AS
+BEGIN
+	SELECT
+		[id] AS JOB_STATE_ID
+	,	[server_name] AS SERVER_NAME
+	,	CONVERT(NVARCHAR(50), job_id) AS JOB_ID
+	,	COALESCE([state], '')	AS [CUSTOM_STATE]		
+	FROM [dbo].[job_state]
+	WHERE [id] = @p_job_state_id;
+END
