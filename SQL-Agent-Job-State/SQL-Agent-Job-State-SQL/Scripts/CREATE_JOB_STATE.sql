cc95b1bb344af5b5c710d209828c60185728fcaf
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-02-16 18:03:51 -0500

    Add setup scripts.

---
+++ SQL-Agent-Job-State/SQL-Agent-Job-State-SQL/Scripts/CREATE_JOB_STATE.sql
@@ -0,0 +1,18 @@
+﻿-- SCRIPT: CREATE_JOB_STATE
+-- this script creates a new row for job state for a SQL Agent job
+
+INSERT [dbo].[job_state] (
+	[server_name]
+,	[job_id]
+,	[state]	
+,	[last_modified]
+)
+VALUES (
+	@@SERVERNAME
+,	'12D9E97B-C2F0-4FAD-801A-143E9B421F38'	-- SAMPLE DATA LOAD with JOB STATE
+,	NULL
+,	GETDATE()
+)
+
+SELECT SCOPE_IDENTITY()
+
