57bc6cf56a62bad3c23609115d6a395c5fbb69a9
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-02-17 19:22:09 -0500

    New project for JOB_CONTROL database.

---
+++ SQL-Agent-Job-State/JOB_CONTROL_DATABASE/Stored Procedures/INITIALIZE_STATE.sql
@@ -0,0 +1,34 @@
+﻿/*
+
+	This procedure should be called as the first step in a SQL Agent job.
+	The purpose is to set the CUSTOM_STATE based on the needs of the job
+	and overwrite any statethat exists from a previous execution of the 
+	SQL Agent job.
+
+*/
+CREATE PROCEDURE [dbo].[INITIALIZE_STATE]
+	@STATE_ID		BIGINT
+,	@CUSTOM_STATE	NVARCHAR(4000) = NULL
+AS
+BEGIN
+	UPDATE [dbo].[STATE]
+	SET
+		CUSTOM_STATE = @CUSTOM_STATE
+	WHERE [STATE_ID] = @STATE_ID;
+
+	IF @@ROWCOUNT = 0
+		INSERT [dbo].[STATE] (
+			[STATE_ID]
+		,	[NAME]
+		,	[CUSTOM_STATE]
+		)
+		VALUES (
+			@STATE_ID
+		,	CONCAT(
+				N'STATE FOR STATE_ID = '
+			,	CONVERT(NVARCHAR(20), @STATE_ID)
+			)
+		,	@CUSTOM_STATE
+		);
+END
+
---
+++ SQL-Agent-Job-State/JOB_CONTROL_DATABASE/Stored Procedures/UPDATE_STATE.sql
@@ -0,0 +1,18 @@
+﻿/*
+	This procedure should be called in each SQL Agent job step where there 
+	are changes to the state that need to be persisted. The thought is that 
+	ths procedure would be called when the SSIS package is done (either successfully
+	or has failed).
+
+*/
+CREATE PROCEDURE [dbo].[UPDATE_STATE]
+	@STATE_ID		BIGINT
+,	@CUSTOM_STATE	NVARCHAR(4000) = NULL
+AS
+BEGIN
+	UPDATE [dbo].[STATE]
+	SET
+		CUSTOM_STATE = @CUSTOM_STATE
+	WHERE [STATE_ID] = @STATE_ID;
+END
+
