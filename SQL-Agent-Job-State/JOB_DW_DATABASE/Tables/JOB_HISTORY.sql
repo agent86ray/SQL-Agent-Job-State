6ea6bb2bc0aa7f4b6d85fa6e42c9f427c7348205
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-02-22 20:47:31 -0500

    Add JOB_HISTORY table and GET_START_JOB_INSTANCE_ID stored procedure.

---
+++ SQL-Agent-Job-State/JOB_DW_DATABASE/Tables/JOB_HISTORY.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[JOB_HISTORY]
+(
+    [instance_id] int,
+    [job_id] uniqueidentifier,
+    [step_id] int,
+    [step_name] nvarchar(128),
+    [sql_message_id] int,
+    [sql_severity] int,
+    [message] nvarchar(4000),
+    [run_status] int,
+    [run_date] int,
+    [run_time] int,
+    [run_duration] int,
+    [operator_id_emailed] int,
+    [operator_id_netsent] int,
+    [operator_id_paged] int,
+    [retries_attempted] int,
+    [server] nvarchar(128)
+)
