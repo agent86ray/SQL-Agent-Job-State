47d138e994de71947741706705cb76d40c278548
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-02-23 08:09:57 -0500

    Add stored procedure GET_ROWS_TO_LOAD_FROM_SYSJOBS
    Retrieve rows from [msdb].[dbo].[sysjobhistory] based on [instance_id] range.

---
+++ SQL-Agent-Job-State/JOB_DW_DATABASE/Stored Procedures/GET_ROWS_TO_LOAD_FROM_SYSJOBS.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[GET_ROWS_TO_LOAD_FROM_SYSJOBS]
+	@START_JOB_INSTANCE_ID	INT
+,	@END_JOB_INSTANCE_ID	INT
+AS
+BEGIN
+	SELECT
+		[instance_id]
+	,	[job_id]
+	,	[step_id]
+	,	[step_name]
+	,	[sql_message_id]
+	,	[sql_severity]
+	,	[message]
+	,	[run_status]
+	,	[run_date]
+	,	[run_time]
+	,	[run_duration]
+	,	[operator_id_emailed]
+	,	[operator_id_netsent]
+	,	[operator_id_paged]
+	,	[retries_attempted]
+	,	[server]
+	FROM [msdb].[dbo].[sysjobhistory]
+	WHERE [instance_id] 
+		BETWEEN @START_JOB_INSTANCE_ID AND @END_JOB_INSTANCE_ID;
+END
